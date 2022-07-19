8501dab665f7681698b8d263a153190804d1374d
Vieran <vieran.nijjar@bjss.com>
2022-07-19 17:32:09 +0100

    made selenium script to get links to all matches, need to refactor and test

--- cricinfo_scraper/cricinfo_scraper.py
+++ cricinfo_scraper/cricinfo_scraper.py
@@ -5,6 +5,7 @@ Scrape IPL data from cricinfo website
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
@@ -13,5 +14,9 @@ chrome_options.add_argument("--headless")
 with webdriver.Chrome(
     service=Service(ChromeDriverManager().install()), options=chrome_options
 ) as driver:
-    driver.get("http://www.python.org")
-    assert "Python" in driver.title
+    driver.get("https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/match-results")
+    assert "cricinfo" in driver.title
+    links = driver.find_elements(By.CSS_SELECTOR, "a[href^='/series/indian-premier-league-2022-1298423/'][href$='full-scorecard']")
+    for link in links:
+        print(link.get_attribute("href"))
+    print(len(links))
