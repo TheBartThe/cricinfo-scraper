bc24e97997598c86ce00f6cbdb2fe6774e69ab09
Vieran <vieran.nijjar@bjss.com>
2022-07-20 16:20:02 +0100

    set up selenium to get all batters data from individual scorecard, including dismissal method. Probably has too many nested try blocks

--- cricinfo_scraper/cricinfo_scraper.py
+++ cricinfo_scraper/cricinfo_scraper.py
@@ -16,19 +16,29 @@ with webdriver.Chrome(
     service=Service(ChromeDriverManager().install()), options=chrome_options
 ) as driver:
     driver.get("https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/gujarat-titans-vs-rajasthan-royals-final-1312200/full-scorecard")
-    columns = driver.find_element(By.CSS_SELECTOR, "table.ds-w-full.ds-table.ds-table-xs.ds-table-fixed.ci-scorecard-table thead tr.ds-border-b.ds-border-line.ds-text-tight-s")
+
+    columns = driver.find_element(By.CSS_SELECTOR, "table.ds-w-full.ds-table.ds-table-xs.ds-table-fixed.ci-scorecard-table:first-of-type thead tr.ds-border-b.ds-border-line.ds-text-tight-s")
     columns = columns.find_elements(By.CSS_SELECTOR, "th")
-    del columns[1]
     columns = [element.get_attribute("innerHTML") for element in columns]
-    batters = driver.find_elements(By.CSS_SELECTOR, "table.ds-w-full.ds-table.ds-table-xs.ds-table-fixed.ci-scorecard-table tbody tr.ds-border-b.ds-border-line.ds-text-tight-s")
     print(",".join(columns))
+
+    batters = driver.find_elements(By.CSS_SELECTOR, "table.ds-w-full.ds-table.ds-table-xs.ds-table-fixed.ci-scorecard-table tbody tr.ds-border-b.ds-border-line.ds-text-tight-s")
     for batter in batters:
         try:
-            name = batter.find_element(By.CSS_SELECTOR, "span.ds-inline-flex.ds-items-center.ds-leading-none a.ds-text-ui-typo.ds-underline.ds-underline-offset-4.ds-decoration-ui-stroke").get_attribute("title")
-            #runs, balls, minutes, fours, sixes, strike_rate = (element.text for element in batter.find_elements(By.CSS_SELECTOR, "td.ds-min-w-max.ds-text-right"))
+            name = batter.find_element(By.CSS_SELECTOR, "td.ds-min-w-max > span.ds-inline-flex.ds-items-center.ds-leading-none > a.ds-text-ui-typo.ds-underline.ds-underline-offset-4.ds-decoration-ui-stroke").get_attribute("title")
+            #dismissal = batter.find_element(By.CSS_SELECTOR, "td.ds-min-w-max > span.ds-flex.ds-cursor-pointer.ds-items-center > span").get_attribute("innerHTML")
+            #dismissal = batter.find_element(By.CSS_SELECTOR, "td.ds-min-w-max:nth-of-type(2)").get_attribute("innerHTML")
             runs = batter.find_element(By.CSS_SELECTOR, "td.ds-min-w-max.ds-text-right strong").get_attribute("innerHTML")
             content = [element.get_attribute("innerHTML") for element in batter.find_elements(By.CSS_SELECTOR, "td.ds-min-w-max.ds-text-right")[1:]]
+            try:
+                dismissal = batter.find_element(By.CSS_SELECTOR, "td.ds-min-w-max > span.ds-flex.ds-cursor-pointer.ds-items-center > span").get_attribute("innerHTML")
+            except NoSuchElementException:
+                try:
+                    dismissal = batter.find_element(By.CSS_SELECTOR, "td.ds-min-w-max:nth-of-type(2)").get_attribute("innerHTML")
+                except NoSuchElementException:
+                    raise
             content.insert(0, runs)
+            content.insert(0, dismissal)
             content.insert(0, name)
         except NoSuchElementException:
             pass
