b5f17b3ef91358b5c34d2b9d07780c5749d23a8f
Vieran <vieran.nijjar@bjss.com>
2022-07-21 15:41:44 +0100

    added docstring to pandas scraping module and turned it into a .py file

--- cricinfo_scraper/pandas_scraper.py
+++ cricinfo_scraper/pandas_scraper.py
@@ -1,5 +1,8 @@
+"""
+Collects data from a cricinfo scorecard and outputs it into a csv
+"""
+
 import pandas as pd
-import lxml
 
 tables = pd.read_html("https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/gujarat-titans-vs-rajasthan-royals-final-1312200/full-scorecard", match="BATTING")
 df = pd.concat(tables)
