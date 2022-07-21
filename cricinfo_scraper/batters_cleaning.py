ef8c39f03efe82a4ffe0cacc4ecf4d49c8f6f512
Vieran <vieran.nijjar@bjss.com>
2022-07-21 16:54:40 +0100

    moved cleaning script into a function with a docstring

--- cricinfo_scraper/batters_cleaning.py
+++ cricinfo_scraper/batters_cleaning.py
@@ -1,20 +1,36 @@
 """
 Cleans the data from the scorecards collected from the cricinfo website
+
+Functions:
+
+    clean_batters_dataframe(DataFrame) -> DataFrame
 """
 
 import pandas as pd
+from pandas import DataFrame
 
-df = pd.read_csv("./data/batters.csv")
-df.drop(columns=["Unnamed: 0", "Unnamed: 8", "Unnamed: 9"], inplace=True)
-df.dropna(subset=["BATTING", "B"], inplace=True)
-df = df.loc[~df["BATTING"].str.startswith(("Did not bat", "Fall of wickets"))]
-df.reset_index(drop=True, inplace=True)
-df.rename(columns={"BATTING": "Batting",
-                  "Unnamed: 1": "Dismissal",
-                  "R": "Runs",
-                  "B": "Balls",
-                  "M": "Minutes",
-                  "SR": "StrikeRate"}, inplace=True)
-df["Out"] = df["Dismissal"] != "not out"
-df["Batting"] = df["Batting"].str.replace("(†|\(c\))","", regex=True).str.strip()
-print(df)
+#df: DataFrame = pd.read_csv("./data/batters.csv")
+
+def clean_batters_dataframe(df: DataFrame) -> DataFrame:
+    """
+    Cleans and return a dataframe of batters.
+
+            Parameters:
+                    df (DataFrame): A dataframe of batters from a scorecard
+
+            Returns:
+                    df (DataFrame): A cleaned version of the input dataframe
+    """
+    df.drop(columns=["Unnamed: 0", "Unnamed: 8", "Unnamed: 9"], inplace=True)
+    df.dropna(subset=["BATTING", "B"], inplace=True)
+    df = df.loc[~df["BATTING"].str.startswith(("Did not bat", "Fall of wickets"))]
+    df.reset_index(drop=True, inplace=True)
+    df.rename(columns={"BATTING": "Batting",
+                    "Unnamed: 1": "Dismissal",
+                    "R": "Runs",
+                    "B": "Balls",
+                    "M": "Minutes",
+                    "SR": "StrikeRate"}, inplace=True)
+    df["Out"] = df["Dismissal"] != "not out"
+    df["Batting"] = df["Batting"].str.replace("(†|\(c\))","", regex=True).str.strip()
+    return df
