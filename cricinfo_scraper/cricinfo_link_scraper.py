eba22c5809824b33715dc85fda9eaeb83bc09cc8
Vieran <vieran.nijjar@bjss.com>
2022-07-22 15:08:48 +0100

    put link scraper into function with docstring

--- cricinfo_scraper/cricinfo_link_scraper.py
+++ cricinfo_scraper/cricinfo_link_scraper.py
@@ -1,5 +1,8 @@
 """
 Scrape IPL data from cricinfo website
+
+Functions:
+    get_links() -> List[str]
 """
 
 from selenium import webdriver
@@ -7,21 +10,27 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from typing import List
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
 
-with webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()), options=chrome_options
-) as driver:
-    driver.get(
-        "https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/match-results"
-    )
-    assert "cricinfo" in driver.title
-    links = driver.find_elements(
-        By.CSS_SELECTOR,
-        "a[href^='/series/indian-premier-league-2022-1298423/'][href$='full-scorecard']",
-    )
-    for link in links:
-        print(link.get_attribute("href"))
-    print(len(links))
+def get_links() -> List[str]:
+    """
+    Gets links from cricinfo website to scorecards.
+            Returns:
+                    links (List[str]): A list of links to scorecards
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+
+    with webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()), options=chrome_options
+    ) as driver:
+        driver.get(
+            "https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/match-results"
+        )
+        assert "cricinfo" in driver.title
+        links: List[str] = driver.find_elements(
+            By.CSS_SELECTOR,
+            "a[href^='/series/indian-premier-league-2022-1298423/'][href$='full-scorecard']",
+        )
+        return links
