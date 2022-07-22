7a0d6eabf598400ee72bb2a76f3b9cbf89c18326
Vieran <vieran.nijjar@bjss.com>
2022-07-22 12:24:07 +0100

    made function which takes in a dataframe of batters scorecards, and returns a dataframe with the totals for each batter

---
+++ cricinfo_scraper/batters_totals.py
@@ -0,0 +1,27 @@
+"""
+Creates a dataframe with the totals for each batter
+
+Functions:
+
+    batters_totals(DataFrame) -> DataFrame
+"""
+
+
+import pandas as pd
+from pandas import DataFrame
+
+
+def batters_totals(df: DataFrame) -> DataFrame:
+    """
+    Returns a dataframe of each batters totals
+
+            Parameters:
+                    df (DataFrame): A cleaned dataframe of batters from a scorecard
+
+            Returns:
+                    totals_df (DataFrame): A dataframe with the totals for each batter
+    """
+    totals_df: DataFrame = df.groupby("Batter", as_index=False)[
+        ["Runs", "Balls", "Fours", "Sixes", "Out"]
+    ].sum()
+    return totals_df
