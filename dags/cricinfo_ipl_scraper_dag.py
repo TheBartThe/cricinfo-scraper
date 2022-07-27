a0ccb1c7afaee8a1d767da829d0b4488c0041c3e
Vieran <vieran.nijjar@bjss.com>
2022-07-27 15:38:02 +0100

    got airflow dag working

--- dags/cricinfo_ipl_scraper_dag.py
+++ dags/cricinfo_ipl_scraper_dag.py
@@ -4,6 +4,13 @@ import airflow
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+
+import os, sys
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
 from cricinfo_scraper.cricinfo_link_scraper import get_links
 from cricinfo_scraper.pandas_scraper import get_all_scorecards
 from cricinfo_scraper.batters_cleaning import clean_batters_dataframe
@@ -44,6 +51,7 @@ store_raw_scorecard_csv = PythonOperator(
 
 def _store_cleaned_scorecard(ti):
     import pandas as pd
+
     df: pd.DataFrame = pd.read_csv("./data/batters.csv", index_col="Unnamed: 0")
     df = clean_batters_dataframe(df)
     df.to_csv("./data/batters_cleaned.csv")
@@ -58,6 +66,7 @@ store_cleaned_scorecard_csv = PythonOperator(
 
 def _store_batters_totals():
     import pandas as pd
+
     df = pd.read_csv("./data/batters_cleaned.csv")
     df = batters_totals(df)
     df.to_csv("./data/batters_totals.csv")
@@ -76,4 +85,10 @@ notify = BashOperator(
     dag=dag,
 )
 
-get_ipl_links >> store_raw_scorecard_csv >> store_cleaned_scorecard_csv >> store_batters_totals_csv >> notify
+(
+    get_ipl_links
+    >> store_raw_scorecard_csv
+    >> store_cleaned_scorecard_csv
+    >> store_batters_totals_csv
+    >> notify
+)
